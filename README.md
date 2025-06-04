65241a26c56db36e1ac52d92b6230a7f6b1f71c8
Dan Serfaty <dan@castle.associates>
2025-06-04 10:59:35 -0700

    Enable Async on app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# tunescout
-Spring Boot Music Recommendation Application
+# Tunescout
+A Spring Boot Music Recommendation Application integrating Spring AI with Ollama
+
+## TODO:
+- Integrate Swagger-UI, see: https://medium.com/@ruwanpradeep9911/implementing-swagger-with-spring-boot-a-step-by-step-guide-4b121e607bd1
+- 
--- src/main/java/com/dev1023/tunescout/TunescoutApplication.java
+++ src/main/java/com/dev1023/tunescout/TunescoutApplication.java
@@ -2,12 +2,13 @@ package com.dev1023.tunescout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TunescoutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TunescoutApplication.class, args);
 	}
-
 }
